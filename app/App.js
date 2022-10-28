27c1f504ecee8a121f5bf66bdc4108e590cf117b
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-10-27 20:40:56 -0400

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,48 +1,28 @@
-import { Card, Row, Col } from "antd";
-import Movie from "./Movie";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-  const movieList = [
-    {
-      title: "The Godfather",
-      year: "1972",
-      genre: "Drama",
-      director: "Francis Ford Capolla",
-    },
-    {
-      title: "Superbad",
-      year: "2007",
-      genre: "Comedy",
-      director: "Greg Mottola",
-    },
-    {
-      title: "The Departed",
-      year: "2006",
-      genre: "Drama",
-      director: "Martin Scorsese",
-    },
-    {
-      title: "Saving Private Ryan",
-      year: "1998",
-      genre: "Action",
-      director: "Steven Spielberg",
-    },
-    {
-      title: "The Expendables",
-      year: "2010",
-      genre: "Action",
-      director: "Sylvester Stallone",
-    },
-  ];
   return (
-    <div>
-      <div className="site-card-wrapper"></div>
-      <Row gutter={16}>
-        {movieList.map((movieObj) => {
-          return <Movie title={movieObj.title} genre={movieObj.genre} />;
-        })}
-        <Movie />,
-      </Row>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;
+ 
+
